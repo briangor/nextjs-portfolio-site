3fc96975ad6aed5922bad5d8b6a0fb72021bce01
briangor <briangor13@gmail.com>
2025-10-02 16:08:48 +0300

    update navbar.tsx

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,5 +1,16 @@
+'use client';
+import styles from '../styles/styles.module.css';
+import LogoDevIcon from '@mui/icons-material/LogoDev';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+
 export default function Navbar() {
   return (
-    <div>John Doe</div>
+    <div className={styles.navbar}>
+      <div>
+        <LogoDevIcon />
+        <span>John Doe</span>
+      </div>
+      <div><MenuRoundedIcon /></div>
+    </div>
   )
 }
