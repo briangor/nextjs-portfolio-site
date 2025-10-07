1a85ccf4a7a194f0eeb5dec8d1081797e34f75d7
briangor <briangor13@gmail.com>
2025-10-07 21:59:48 +0300

    update navbar.tsx

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,13 +4,24 @@ import LogoDevIcon from '@mui/icons-material/LogoDev';
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 
 export default function Navbar() {
+
+  // todo: add toggle functionality for mobile menu
+
   return (
-    <div className={styles.navbar}>
-      <div>
+    <nav className={styles.navbar}>
+      <div className={styles.navbar__logo}>
         <LogoDevIcon />
         <span>John Doe</span>
       </div>
-      <div><MenuRoundedIcon /></div>
-    </div>
+      <ul className={styles.navbar__menu}>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <div className={styles.navbar__toggle}>
+        <MenuRoundedIcon />
+      </div>
+    </nav>
   )
 }
