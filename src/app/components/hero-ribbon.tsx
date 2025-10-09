c6e04a4987fe848248cddd9fd1530ff4cab81dca
briangor <briangor13@gmail.com>
2025-10-09 22:07:49 +0300

    add hero-ribbon.tsx

---
+++ src/app/components/hero-ribbon.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function HeroRibbon() {
+  return (
+    <div>
+        <ul>
+            <li>Email</li>
+            <li>LinkedIn</li>
+            <li>GitHub</li>
+        </ul>
+    </div>
+  )
+}
