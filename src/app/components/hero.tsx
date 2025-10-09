ef4f15b67fe1e22f63d4e995755ec378e0cd1ccd
briangor <briangor13@gmail.com>
2025-10-09 22:07:36 +0300

    add hero.tsx

---
+++ src/app/components/hero.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div>
+        <h2>John D. Doe</h2>
+        <h1>A Seasoned <b><i>Java <br/> Developer</i></b></h1>
+        <p>based in Nairobi, Kenya</p>
+        <p>
+            <span>Spring Boot</span>
+            <span>|</span>
+            <span>Microservices</span>
+            <span>|</span>
+            <span>REST APIs</span>
+        </p>
+
+        <button>Check out my work</button>
+    </div>
+  )
+}
